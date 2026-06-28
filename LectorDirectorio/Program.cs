50790af866bfc5a2576d11d59553f608fc6a060c
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-06-28 12:21:52 -0300

    Valido la existencia del path

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+string path;
+
+do
+{
+    Console.WriteLine("Ingrese un path válido de un directorio para analizar:");
+    path = Console.ReadLine();
+} while (!Directory.Exists(path));
+
