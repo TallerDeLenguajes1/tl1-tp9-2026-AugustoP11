feb51210ac11f14f582a4ba197eef0654c62dd06
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-06-28 16:17:29 -0300

    Termino ejercicio 1

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -6,6 +6,7 @@ do
 {
     Console.WriteLine("Ingrese un path válido de un directorio para analizar:");
     path = Console.ReadLine();
+    path = $@"{path}";
 } while (!Directory.Exists(path));
 
 DirectoryInfo informacionDirectorio = new DirectoryInfo(path);
@@ -31,4 +32,27 @@ foreach (FileInfo archivo in archivos)
     double tamanioKB = archivo.Length / 1024.0;
 
     Console.WriteLine($"{archivo.Name} | {tamanioKB:F2} KB");
+}
+
+string archivoReporte = path + @"/reporte_archivos.csv";
+
+if (!File.Exists(archivoReporte))
+{
+    using (FileStream fs = new FileStream(archivoReporte, FileMode.Create))
+    {
+    }
+}
+
+string pathActual = @"e:/GitHub/Taller-de-lenguajes-1/tl1-tp9-2026-AugustoP11/LectorDirectorio";
+string pathRelativo = Path.GetRelativePath(pathActual, archivoReporte);
+
+
+using (StreamWriter escribirReporte = new StreamWriter(archivoReporte))
+{
+    escribirReporte.WriteLine("Nombre del archivo;Tamaño (KB);Ultima modificación");
+    foreach (FileInfo archivo in archivos)
+    {
+        double tamanioKB = archivo.Length / 1024.0;
+        escribirReporte.WriteLine($"{archivo.Name};{tamanioKB:F2};{archivo.LastWriteTime}");
+    }
 }
