576b421c54ae67c3f129f6c7191d5932529ed590
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-06-28 13:03:28 -0300

    Listo las carpetas y archivos

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -8,3 +8,27 @@ do
     path = Console.ReadLine();
 } while (!Directory.Exists(path));
 
+DirectoryInfo informacionDirectorio = new DirectoryInfo(path);
+
+//Lo hice de esta forma primero, pero quiero probar con DirectoryInfo y FileInfo
+//string[] carpetas = Directory.GetDirectories(path);
+
+DirectoryInfo[] subdirectorios = informacionDirectorio.GetDirectories();
+
+Console.WriteLine("CARPETAS ENCONTRADAS:");
+
+foreach (DirectoryInfo carpeta in subdirectorios)
+{
+    Console.WriteLine($"- {carpeta.Name}");
+}
+
+Console.WriteLine("\nARCHIVOS ENCONTRADOS:");
+Console.WriteLine("|---NOMBRE---|---TAMAÑO---|");
+FileInfo[] archivos = informacionDirectorio.GetFiles();
+
+foreach (FileInfo archivo in archivos)
+{
+    double tamanioKB = archivo.Length / 1024.0;
+
+    Console.WriteLine($"{archivo.Name} | {tamanioKB:F2} KB");
+}
