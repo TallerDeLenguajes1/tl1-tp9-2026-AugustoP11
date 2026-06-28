6c330b8953b79fd1722efd21412a78d526a02617
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-06-28 17:08:59 -0300

    Solicito path cancion

--- LectorTagMP3/Program.cs
+++ LectorTagMP3/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+string pathCancion;
+
+do
+{
+    Console.WriteLine("Ingrese la ruta de la cancion para leer su TAG:");
+    pathCancion = Console.ReadLine();
+    pathCancion = $@"{pathCancion}";
+} while (!File.Exists(pathCancion));
+
+using (FileStream fs = new FileStream(pathCancion, FileMode.Open))
+{
+    
+}
